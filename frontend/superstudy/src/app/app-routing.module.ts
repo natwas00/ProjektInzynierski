6bacbec02373eb55528fccc13527173b47a26ed5
Natalia Wasiak <97039676+agent20008973@users.noreply.github.com>
2022-05-12 15:52:33 +0200

    Add files via upload

--- frontend/superstudy/src/app/app-routing.module.ts
+++ frontend/superstudy/src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardTeacherComponent } from './board-teacher/board-teacher.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
-
+import { ChangePassComponent } from './change_pass/change-pass.component';
+import { AuthGuard} from './_services/auth.guards';
 const routes: Routes = [
+  {path: 'change_pass', component: ChangePassComponent, canActivate: [AuthGuard]},
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'user', component: BoardUserComponent },
-  { path: 'mod', component: BoardModeratorComponent },
-  { path: 'admin', component: BoardAdminComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: BoardUserComponent},
+  { path: 'teacher', component: BoardTeacherComponent},
+  { path: 'admin', component: BoardAdminComponent  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
