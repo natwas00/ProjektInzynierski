6f8c830009360bb5c9cac0ff154779121ab2e3a5
Natalia Wasiak <97039676+agent20008973@users.noreply.github.com>
2022-05-12 16:01:40 +0200

    Add files via upload

--- frontend/superstudy/src/app/board-admin/board-admin.component.ts
+++ frontend/superstudy/src/app/board-admin/board-admin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
-
+import { TokenStorageService } from '../_services/token-storage.service';
+import {
+  Router
+} from "@angular/router";
 @Component({
   selector: 'app-board-admin',
   templateUrl: './board-admin.component.html',
@@ -8,9 +11,17 @@ import { UserService } from '../_services/user.service';
 })
 export class BoardAdminComponent implements OnInit {
   content?: string;
-  constructor(private userService: UserService) {}
-
+  constructor(private userService: UserService, private tokenStorageService: TokenStorageService, private router: Router) {}
+  private roles: string;
   ngOnInit(): void {
+    const user = this.tokenStorageService.getUser();
+    this.roles = user.roles;
+    if (this.roles.includes('ROLE_USER')){
+      this.router.navigate(['/user']);
+      }
+  if (this.roles.includes('ROLE_TEACHER')){
+    this.router.navigate(['/teacher']);
+      }
     this.userService.getAdminBoard().subscribe(
       (data) => {
         this.content = data;
