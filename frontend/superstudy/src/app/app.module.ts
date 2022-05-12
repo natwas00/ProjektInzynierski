26dba79a7369f2ed63f3ab74b959c18cc4385378
Natalia Wasiak <97039676+agent20008973@users.noreply.github.com>
2022-05-12 16:00:10 +0200

    Add files via upload

--- frontend/superstudy/src/app/app.module.ts
+++ frontend/superstudy/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +10,18 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardTeacherComponent } from './board-teacher/board-teacher.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-
+import { AuthGuard } from './_services/auth.guards';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
-
+import { ChangePassComponent } from './change_pass/change-pass.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,11 +30,21 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     HomeComponent,
     ProfileComponent,
     BoardAdminComponent,
-    BoardModeratorComponent,
+    BoardTeacherComponent,
     BoardUserComponent,
+    ChangePassComponent,
+
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [authInterceptorProviders],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    NgbModule,
+    ShowHidePasswordModule
+],
+    
+  providers: [authInterceptorProviders, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
