e9a0cd110667d4fdd8bcd3e7d74a91e67672050f
Natalia Wasiak <97039676+agent20008973@users.noreply.github.com>
2022-05-13 13:53:44 +0200

    Add files via upload

---
+++ frontend/superstudy/src/app/board-teacher/board-teacher.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardTeacherComponent } from './board-teacher.component';
+
+describe('BoardTeacherComponent', () => {
+  let component: BoardTeacherComponent;
+  let fixture: ComponentFixture<BoardTeacherComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoardTeacherComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardTeacherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/superstudy/src/app/board-teacher/board-teacher.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+import {
+  Router
+} from "@angular/router";
+@Component({
+  selector: 'app-board-teacher',
+  templateUrl: './board-teacher.component.html',
+  styleUrls: ['./board-teacher.component.scss'],
+})
+
+export class BoardTeacherComponent implements OnInit {
+  content?: string;
+  constructor(private userService: UserService,private tokenStorageService: TokenStorageService, private router: Router) {}
+  private roles: string;
+  ngOnInit(): void {
+    const user = this.tokenStorageService.getUser();
+    this.roles = user.roles;
+    if (this.roles.includes('ROLE_USER')){
+      this.router.navigate(['/user']);
+  }
+  
+    this.userService.getTeacherBoard().subscribe(
+      (data) => {
+        this.content = data;
+      },
+      (err) => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
+  }
+}
