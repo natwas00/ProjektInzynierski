e594187b499fe77d5f314743f62459b7f0b37d99
Natalia Wasiak <97039676+agent20008973@users.noreply.github.com>
2022-05-13 13:54:28 +0200

    Add files via upload

--- frontend/superstudy/src/app/board-user/board-user.component.ts
+++ frontend/superstudy/src/app/board-user/board-user.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
 
+import { TokenStorageService } from '../_services/token-storage.service';
+import {
+  Router
+} from "@angular/router";
+
+
 @Component({
   selector: 'app-board-user',
   templateUrl: './board-user.component.html',
   styleUrls: ['./board-user.component.scss'],
 })
 export class BoardUserComponent implements OnInit {
+  form: any = {
+    first_name: null,
+    last_name: null
+    
+  };
+  private roles: string;
+  isSuccessful = false;
   content?: string;
-  constructor(private userService: UserService) {}
-
+  constructor(private userService: UserService, private tokenStorageService: TokenStorageService, private router: Router) {}
+  
   ngOnInit(): void {
+    const user = this.tokenStorageService.getUser();
+    this.roles = user.roles;
+    if (this.roles.includes('ROLE_TEACHER')){
+      this.router.navigate(['/teacher']);
+  }
+  
+    
+    
     this.userService.getUserBoard().subscribe(
       (data) => {
         this.content = data;
@@ -19,5 +40,8 @@ export class BoardUserComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     );
+  }
+  onSubmit(): void {
+   
   }
 }
