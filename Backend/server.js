c5a9af4e08c21bcac5654e7e5f3ac6680460a16e
natwas00 <97039676+natwas00@users.noreply.github.com>
2022-12-05 20:08:45 +0100

    Add files via upload
    Usuwanie przeterminowanych zadań domowych ucznia z bazy

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const nodeCron = require("node-cron");
+const { Op } = require("sequelize");
 const cors = require("cors");
+const { fiszki } = require("./app/models");
 const app = express();
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -14,8 +17,8 @@ const Role = db.role;
 const Set = db.set;
 db.sequelize.sync({force:false}).then(() => { //zmienić na true jeśli pierwszy raz się uruchamia
   console.log('Drop and Resync Db');
-  //initial()
- 
+ // initial()
+  //createSuperstudysets()
 });
 
 // simple route
@@ -27,12 +30,15 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/set.routes')(app);
 require('./app/routes/image.upload.routes')(app);
 require('./app/routes/class.routes')(app);
-
+require('./app/routes/superstudyset.routes')(app);
+require('./app/routes/study.routes')(app);
+require('./app/routes/not.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
 function initial() {
   Role.create({
     id: 1,
@@ -49,3 +55,121 @@ function initial() {
     name: "admin"
   });
 }
+function createSuperstudysets(){
+  Set.create({
+    name: "Animals",
+    subject: "Język angielski",
+    level: "Szkoła podstawowa",
+    points: 40
+  }).then(data => {
+    fiszki.create({
+      first_side: "lew",
+      second_side: "lion",
+      setId: 1//data.id
+    }),
+    fiszki.create({
+      first_side: "słoń",
+      second_side: "elephant",
+      setId: data.id
+    }),
+    fiszki.create({
+      first_side: "kot",
+      second_side: "cat",
+      setId: 1//data.id
+    }),
+    fiszki.create({
+      first_side: "pies",
+      second_side: "dog",
+      setId: 1//data.id
+    }),
+    fiszki.create({
+      first_side: "małpa",
+      second_side: "monkey",
+      setId: 1//data.id
+    }),
+    fiszki.create({
+      first_side: "orzeł",
+      second_side: "eagle",
+      setId: 1//data.id
+    }),
+    fiszki.create({
+      first_side: "ptak",
+      second_side: "bird",
+      setId: 1//data.id
+    })
+    
+  }),
+  Set.create({
+    name: "Colors",
+    subject: "Język angielski",
+    level: "Szkoła podstawowa",
+    points: 20
+  }).then(data=>{
+    fiszki.create({
+      first_side: "różowy",
+      second_side: "pink",
+      setId: 2//data.id
+    }),
+    fiszki.create({
+      first_side: "czerwony",
+      second_side: "red",
+      setId: 2//data.id
+    }),
+    fiszki.create({
+      first_side: "zielony",
+      second_side: "green",
+      setId: 2//data.id
+    }),
+    fiszki.create({
+      first_side: "fioletowy",
+      second_side: "purple",
+      setId: 2//data.id
+    }),
+    fiszki.create({
+      first_side: "czarny",
+      second_side: "black",
+      setId: 2//data.id
+    }),
+    fiszki.create({
+      first_side: "biały",
+      second_side: "white",
+      setId: 2//data.id
+    })
+  }),
+  Set.create({
+    name: "Characters",
+    subject: "Język angielski",
+    level: "Szkoła podstawowa",
+    points: 70
+  }).then(data=>{
+    fiszki.create({
+      first_side: "miły",
+      second_side: "nice",
+      setId: 3//data.id
+    }),
+    fiszki.create({
+      first_side: "niefrasobliwy",
+      second_side: "easy-going",
+      setId: 3//data.id
+    }),
+    fiszki.create({
+      first_side: "towarzyski",
+      second_side: "out-going",
+      setId: 3//data.id
+    }),
+    fiszki.create({
+      first_side: "radosny",
+      second_side: "cheerful",
+      setId: 3//data.id
+    })
+  })
+}
+async function check_date(){
+  const class_task = db.class_task;
+  console.log(new Date().toLocaleDateString())
+  class_task.destroy({where:{ finishDate :  {[Op.lte]:new Date()}}}).then(info=>{
+      console.log("usunięto")
+  })
+}
+const job = nodeCron.schedule("00 00 00 * * *", check_date)
+check_date()
