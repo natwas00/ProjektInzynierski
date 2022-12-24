f7918e902f44b7fa6798e1c028d1ae6af178f8bb
natwas00 <97039676+natwas00@users.noreply.github.com>
2022-12-24 13:10:52 +0100

    Update db.config.js

--- Backend/app/config/db.config.js
+++ Backend/app/config/db.config.js
@@ -11,11 +11,24 @@
 //       idle: 10000
 //     }
 //   };
+// module.exports = {
+//     HOST: "mysql.wmi.amu.edu.pl",
+//     USER: "s452743_proj",
+//     PASSWORD: "ingstanitchanta",
+//     DB: "s452743_proj",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
 module.exports = {
-    HOST: "mysql.wmi.amu.edu.pl",
-    USER: "s452743_proj",
-    PASSWORD: "ingstanitchanta",
-    DB: "s452743_proj",
+    HOST: "sql7.freesqldatabase.com",
+    USER: "sql7586372",
+    PASSWORD: "VtLjIsjtNd",
+    DB: "sql7586372",
     dialect: "mysql",
     pool: {
       max: 5,
